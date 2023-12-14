3a8c3eea9289c130126d5103d79de894657d9e12
Christian Lopez <80645989+Chris9lopz@users.noreply.github.com>
2023-12-14 14:23:49 -0500

    add: ejercicio 3.js

---
+++ clase-javascript/ejercicio3.js
@@ -0,0 +1,11 @@
+//Utiliza una condición booleana para imprimir el mensaje
+
+// Declaración de variables
+let esDiaSoleado = false;
+
+// Condición
+if (esDiaSoleado) {
+    console.log("Hoy es un día soleado");
+} else {
+    console.log("Hoy no es un día soleado");
+}
