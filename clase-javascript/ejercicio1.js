daebbb0d230bda80bc7c484032c77d288e637ff7
Christian Lopez <80645989+Chris9lopz@users.noreply.github.com>
2023-12-14 14:04:53 -0500

    add: ejercicios de tipos de datos primitivos

---
+++ clase-javascript/ejercicio1.js
@@ -0,0 +1,9 @@
+// Realizar la operación arimetica de suma de dos numeros
+
+// Declaración de variables
+let numero1 = 10;
+let numero2 = 20;
+
+// Operación aritmética
+let suma = numero1 + numero2;
+console.log(suma);
---
+++ clase-javascript/ejercicio2.js
@@ -0,0 +1,11 @@
+// Combinar dos cadenas de texto y muestre el resultado por consola.
+
+// Declaración de variables
+let cadena1 = "Hola";
+let cadena2 = "Mundo";
+
+// Concatenación de cadenas
+let cadena = cadena1 + " " + cadena2;
+
+// Mostrar resultado por consola
+console.log(cadena);
