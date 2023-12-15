12d1b790b96ba0ba801cb43e1f224f59599b55df
Christian Lopez <80645989+Chris9lopz@users.noreply.github.com>
2023-12-15 14:17:49 -0500

    add: iteración en una sola linea de código

--- clase-javascript/funciones-e4.js
+++ clase-javascript/funciones-e4.js
@@ -1,8 +1,12 @@
 // Crear una función que me imprima en consola los numero del 1 al n (n es un parámetro de la función)
 
+let frase = '';
+
 function imprimirNumeros(n) {
     for(let i = 1; i <= n; i++) {
-        console.log(i);
+        frase += i + ' ';
     }
+    console.log(frase);
 }
 
+imprimirNumeros(10);
