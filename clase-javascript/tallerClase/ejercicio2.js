4df9de7f5d4c4a788e1ef94939ca53f6b8f88a4d
Christian Lopez <80645989+Chris9lopz@users.noreply.github.com>
2023-12-15 21:01:11 -0500

    fix: error a la hora de calcular el numero primo

--- clase-javascript/tallerClase/ejercicio2.js
+++ clase-javascript/tallerClase/ejercicio2.js
@@ -1,35 +1,37 @@
 // Generar un array con los primeros n números primos.
-function esPrimo(n){
-    if(n > 1) {
-        
-        if(n == 2){
-            return true;
-        }
-
-        for(let i = 2; i < n; i++){
-            if(n % i == 0) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-    }
+function esPrimo(n) {
+	if (n > 1) {
+		if (n == 2) {
+			return true;
+		}
+		let cont = 0;
+		for (let i = 2; i < n; i++) {
+			if (n % i == 0) {
+				cont += 1;
+			}
+		}
+		if (cont > 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
 
-function primerNP(indice){
-    let numero = [];
-    let contador = 1;
+console.log(esPrimo(15));
 
-    while(numero.length != indice) {
-        if (esPrimo(contador)) {
-            numero.push(contador);
-    }
-    contador++;
-}
-    return numero;
+function primerNP(indice) {
+	let numero = [];
+	let contador = 1;
+
+	while (numero.length != indice) {
+		if (esPrimo(contador)) {
+			numero.push(contador);
+		}
+		contador++;
+	}
+	return numero;
 }
 
 let resultado = primerNP(15);
 console.log(resultado);
-
-
